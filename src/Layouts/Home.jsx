32a7226f44ae9c7026586c88050ca9b565195df0
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-11-14 00:10:03 +0600

    header added

---
+++ src/Layouts/Home.jsx
@@ -0,0 +1,17 @@
+import Header from "../Components/Header/Header";
+
+export default function Home() {
+  return (
+    <div className="w-11/12">
+      <header>
+        <Header></Header>
+      </header>
+      <nav>
+
+      </nav>
+      <main>
+
+      </main>
+    </div>
+  )
+}
