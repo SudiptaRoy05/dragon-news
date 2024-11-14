c763e4a2a26aab272ab67c52e3692d41d07b3e4f
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-11-14 14:35:01 +0600

    find us on section added

---
+++ src/Components/FindUs/FindUs.jsx
@@ -0,0 +1,38 @@
+import { FaFacebook } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagramSquare } from "react-icons/fa";
+
+
+export default function FindUs() {
+  return (
+    <div>
+      <h2 className="font-semibold">Find Us On</h2>
+      <div className="mt-3 ml-5 border border-gray-500 rounded-lg font-semibold text-gray-500">
+        <div className=" border-b border-gray-500 py-4 pl-10 hover:bg-base-300">
+          <a className="w-full flex items-center gap-2">
+            <span>
+              <FaFacebook></FaFacebook>
+            </span>
+            <span>Facebook</span>
+          </a>
+        </div>
+        <div className="border-b py-3 pl-10 border-gray-500 hover:bg-base-300">
+          <a className="w-full flex items-center gap-2">
+            <span>
+              <FaSquareXTwitter></FaSquareXTwitter>
+            </span>
+            <span>Twitter</span>
+          </a>
+        </div>
+        <div className=" py-3 pl-10 hover:bg-base-300">
+          <a className="w-full flex items-center gap-2">
+            <span>
+              <FaInstagramSquare></FaInstagramSquare>
+            </span>
+            <span>Instagram</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/Components/LayoutComponent/RightNav/RighhtNav.jsx
+++ src/Components/LayoutComponent/RightNav/RighhtNav.jsx
@@ -1,9 +1,15 @@
+import FindUs from "../../FindUs/FindUs";
 import SocilaLogin from "../../SocilaLogin/SocilaLogin";
 
 export default function RighhtNav() {
   return (
     <div>
-      <SocilaLogin></SocilaLogin>
+      <section>
+        <SocilaLogin></SocilaLogin>
+      </section>
+      <section className="mt-3">
+        <FindUs></FindUs>
+      </section>
     </div>
-  )
+  );
 }
